a7f89e8a4538e01cd98da5d819c4c3edd5aa6ae8
Farhan Shahriar <farhanshahriar500@gmail.com>
2026-09-22 07:49:27 +0600

    Added image_url column and sync schemas

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app = FastAPI(
     title="Housing & Roommate Management Platform"
 )
 
-origins = ["*"]
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
